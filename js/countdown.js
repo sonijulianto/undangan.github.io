e233e98be6cc00e39fcd724755cf25f5b12c9038
unknown <sonijulianto@gmail.com>
2021-01-26 16:26:00 +0700

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,22 @@
+
+var countDownDate = new Date("Feb 28, 2021 15:37:25").getTime();
+
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "&nbsp &nbsp &nbsp " + hours + "&nbsp &nbsp &nbsp " + minutes + "&nbsp &nbsp &nbsp " + seconds + " ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
